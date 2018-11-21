d699b2452edb850b89e4b6987f51700bf01b8b18
Leon <sirliuyang@hotmail.com>
2018-11-21 11:20:30 +0800

    Initial commit

---
+++ src/main/java/com/formula/App.java
@@ -0,0 +1,13 @@
+package com.formula;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
